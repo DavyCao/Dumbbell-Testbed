b5577002d958046f92ba84706b041e9bc331e946
Yi Cao <yicao1@cs.stonybrook.edu>
2018-12-12 14:07:41 -0500

    Add files via upload

--- router-control.py
+++ router-control.py
@@ -19,7 +19,7 @@ filename = "/tmp/experiment_logs/Exp_" + "{:%y%m%d_%H%M%S}".format(d) + ".csv"
 csvfile = open(filename, 'w')
 writer = csv.writer(csvfile)
 writer.writerow(['Cong', 'Loss (%)', 'Delay (ms)', 'BW (mbps)', 'Actual Loss (%)', 'Actual BW (mbps)'])
-data = []
+# data = []
 
 for c in cong:
 	# Set congestion control
@@ -36,56 +36,58 @@ for c in cong:
 	for i in loss:
 		for j in delay:
 			for k in bw:
-				print("")
-				cmd = "./set-network " + str(i) + " " + str(j) + " " + str(k)
-				subprocess.run(cmd, shell=True)
+				try:
+					print("")
+					cmd = "./set-network " + str(i) + " " + str(j) + " " + str(k)
+					subprocess.run(cmd, shell=True)
 
-				# Check the configs
-				qdiscConfig = subprocess.run("tc qdisc show dev ifb0", shell=True, stdout=subprocess.PIPE).stdout
-				classConfig = subprocess.run("tc class show dev ifb0", shell=True, stdout=subprocess.PIPE).stdout
+					# Check the configs
+					qdiscConfig = subprocess.run("tc qdisc show dev ifb0", shell=True, stdout=subprocess.PIPE).stdout
+					classConfig = subprocess.run("tc class show dev ifb0", shell=True, stdout=subprocess.PIPE).stdout
 
-				if len(qdiscConfig) == 0:
-					qdiscConfig = "No qdisc config!\n".encode()
-				if len(classConfig) == 0:
-					classConfig = "No class config!\n".encode()
+					if len(qdiscConfig) == 0:
+						qdiscConfig = "No qdisc config!\n".encode()
+					if len(classConfig) == 0:
+						classConfig = "No class config!\n".encode()
 
-				# Send instructions to Server3 -- sender
-				s3 = socket.socket()
-				s3.connect(('192.168.3.2', 6001))
-				s3.sendall(qdiscConfig)
-				s3.sendall(classConfig)
-				s3.sendall("Start!".encode())
-				s3.close()
+					msg_snd = qdiscConfig + "\n".encode() + classConfig + "\nStart!".encode()
 
-				time.sleep(1)
+					# Send instructions to Server3 -- sender
+					s3 = socket.socket()
+					s3.connect(('192.168.3.2', 6001))
+					s3.sendall(msg_snd)
+					s3.close()
 
-				# Send instructions to Server2 -- receiver
-				s2 = socket.socket()
-				s2.connect(('192.168.2.2', 6001))
-				s2.sendall(qdiscConfig)
-				s2.sendall(classConfig)
-				s2.sendall("Start!".encode())
+					time.sleep(1)
 
-				#print(qdiscConfig.decode())
-				#print(classConfig.decode())
-				print("Cong: " + c + "; Loss: " + str(i) + "%; Delay: " + str(j) + "ms; BW: " + str(k) + "mbps")
+					# Send instructions to Server2 -- receiver
+					s2 = socket.socket()
+					s2.connect(('192.168.2.2', 6001))
+					s2.sendall(msg_snd)
 
-				msg = s2.recv(1024).decode()
-				print(msg)
-				s2.close()
-				
-				actualBW = float(msg.split()[-3])
-				lossNum = float(msg.split()[8])
+					#print(qdiscConfig.decode())
+					#print(classConfig.decode())
+					print("Cong: " + c + "; Loss: " + str(i) + "%; Delay: " + str(j) + "ms; BW: " + str(k) + "mbps")
 
-				transferTime = float(msg.split()[-7].split('-')[1])
-				pktNum = (transferTime * actualBW) / 1448 * 1500
-				actualLoss = lossNum / pktNum
+					msg_recv = s2.recv(1024).decode()
+					print(msg_recv)
+					s2.close()
+					
+					actualBW = float(msg_recv.split()[-3])
+					lossNum = float(msg_recv.split()[8])
 
-				record = [i, j, k, actualLoss, actualBW]
-				record = ['%.3f' % a for a in record]
-				record.insert(0, c)
-				print(record)
-				data.append(record)
+					transferTime = float(msg_recv.split()[-7].split('-')[1])
+					pktNum = (transferTime * actualBW) / 1448 * 1500
+					actualLoss = lossNum / pktNum
+
+					record = [i, j, k, actualLoss, actualBW]
+					record = ['%.3f' % a for a in record]
+					record.insert(0, c)
+					print(record)
+					writer.writerow(record)
+					# data.append(record)
+				except Exception as e:
+					print(e)
 
 # Terminate Server2 and Server3 Listener
 s2 = socket.socket()
@@ -101,5 +103,5 @@ s3.close()
 subprocess.run("./clean-tc", shell=True)
 print("Experiments finish!")
 
-writer.writerows(data)
+# writer.writerows(data)
 csvfile.close()
