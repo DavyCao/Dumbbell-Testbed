1d754351d21b5547bff527a9db547c25c5b384b5
Yi Cao <yicao1@cs.stonybrook.edu>
2019-04-03 18:04:12 -0400

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 from sklearn.externals.six import StringIO
@@ -10,71 +10,123 @@ from IPython.display import Image
 import pydotplus
 
 
-df = pd.read_csv("tbf-exp-190402_200224.csv")
-print(df)
+expName = "tbf-exp-190402_200224"
+csvname_bt = expName + "-bwTree.csv"
+csvname_lt = expName + "-lossTree.csv"
 
-bbrGoodput = {}
-cubicGoodput = {}
-decision = {}
+df = pd.read_csv(expName + ".csv", header=0)
+#print(df)
 
+bbrBw, cubicBw = {}, {}
+bbrLoss, cubicLoss = {}, {}
+bwTree, lossTree = {}, {}
+bbrKeys, cubicKeys, commonKeys = set(), set(), set()
+
+# Show the data size
 rows, cols = df.shape
-for i in range(rows):
-    rowData = df.iloc[i]
-    key = str(rowData[' Delay']) + "-" + str(rowData[' BW']) + "-" + str(rowData[' Limit'])
+print('Data size: ' + str(df.shape))
 
-    if rowData['CC'] == 'bbr':
-        bbrGoodput[key] = rowData[' Goodput(bps)']
-    elif rowData['CC'] == 'cubic':
-        cubicGoodput[key] = rowData[' Goodput(bps)']
+# Class Label: 0->bbr, 1->cubic
+label = {}
+label['bbr'] = 0
+label['cubic'] = 1
+
+# Bw/Loss decision map
+def mapping():
+    for i in range(rows):
+        rowData = df.iloc[i]
+        #print(rowData)
+        key = str(rowData[' Delay']) + "-" + str(rowData[' BW']) + "-" + str(rowData[' Limit'])
+
+        if rowData['CC'] == 'bbr':
+            bbrKeys.add(key)
+            bbrBw[key], bbrLoss[key] = rowData[' Goodput(bps)'], rowData[' Loss(%)']
+        elif rowData['CC'] == 'cubic':
+            cubicKeys.add(key)
+            cubicBw[key], cubicLoss[key] = rowData[' Goodput(bps)'], rowData[' Loss(%)']
+
+    for key in bbrKeys:
+        if key in cubicKeys:
+            commonKeys.add(key)
+            # bw decision
+            if bbrBw[key] < cubicBw[key]:
+                bwTree[key] = label['cubic']
+            else:
+                bwTree[key] = label['bbr']
+            # loss decision
+            if bbrLoss[key] < cubicLoss[key]:
+                lossTree[key] = label['bbr']
+            else:
+                lossTree[key] = label['cubic']
 
 
-for k in bbrGoodput.keys():
-    if k in cubicGoodput.keys():
-        if bbrGoodput[k] < cubicGoodput[k]:
-            decision[k] = 'cubic'
-        else:
-            decision[k] = 'bbr'
+def treeCSV():
+    # Creating the bwTree/lossTree (csv)
+    csvfile_bt = open(csvname_bt, 'w')
+    csvfile_lt = open(csvname_lt, 'w')
+    writer_bt = csv.writer(csvfile_bt)
+    writer_lt = csv.writer(csvfile_lt)
 
+    header = ['Delay', 'Bw', 'Buffer', 'Decision']
+    writer_bt.writerow(header)
+    writer_lt.writerow(header)
 
-# Creating the decision tree (csv)
-csvname = "tbf-exp-190402_200224-decisionTree.csv"
-csvfile = open(csvname, 'w')
-writer = csv.writer(csvfile)
-header = ['Delay(ms)', 'BW(mbps)', 'Buffer(bytes)', 'Decision']
-writer.writerow(header)
+    for key in commonKeys:
+        tokens = key.split('-')
+        record_bt = [tokens[0], tokens[1], tokens[2], bwTree[key]]
+        record_lt = [tokens[0], tokens[1], tokens[2], lossTree[key]]
 
-for k in decision.keys():
-    tokens = k.split('-')
-    record = [tokens[0], tokens[1], tokens[2], decision[k]]
-    writer.writerow(record)
+        writer_bt.writerow(record_bt)
+        writer_lt.writerow(record_lt)
 
-csvfile.close()
+    csvfile_bt.close()
+    csvfile_lt.close()
+
 
 # Creating the decision tree (model)
-df_Dtree = pd.read_csv("tbf-exp-190402_200224-decisionTree.csv")
-feature_cols = ['Delay(ms)', 'BW(mbps)', 'Buffer(bytes)']
-X = df_Dtree[feature_cols]
-y = df_Dtree.Decision
+def dtModel(filename, metric):
+    df_Dtree = pd.read_csv(filename, header=0)
+    feature_cols = ['Delay', 'Bw', 'Buffer']
+    X = df_Dtree[feature_cols]
+    y = df_Dtree.Decision
+    # print(X)
+    # print(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    clf = DecisionTreeClassifier()
+    # Regression does not apply here
+    # clf = DecisionTreeRegressor()
+    clf = clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
 
-clf = DecisionTreeClassifier()
-clf = clf.fit(X_train,y_train)
-y_pred = clf.predict(X_test)
+    print("Model accuracy (" + metric + "): " + str(metrics.accuracy_score(y_test, y_pred)))
+    return [clf, feature_cols]
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
 # Plot
-dot_data = StringIO()
+def plotTree(clf, feature_cols, metric):
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data,
+                    filled=True, rounded=True,
+                    special_characters=True,
+                    feature_names=feature_cols,
+                    # max_depth=5,
+                    class_names=['bbr', 'cubic']
+                    )
 
-export_graphviz(clf, out_file=dot_data,
-                filled=True, rounded=True,
-                special_characters=True,
-                feature_names=feature_cols,
-                max_depth=5,
-                class_names=clf.classes_
-                )
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png(expName + "-" + metric + 'Tree.png')
+    Image(graph.create_png())
 
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-graph.write_png('decisionTree.png')
-Image(graph.create_png())
+
+if __name__ == "__main__":
+
+    mapping()
+    treeCSV()
+
+    # Build decision tree and visualize
+    [clf, feature_cols] = dtModel(csvname_bt, 'bw')
+    plotTree(clf, feature_cols, 'bw')
+
+    [clf, feature_cols] = dtModel(csvname_lt, 'loss')
+    plotTree(clf, feature_cols, 'loss')
